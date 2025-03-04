6a0e888bb9ad146fb45bc04c82c6253f1d1ee5a7
ArizSaputra <as908244@gmail.com>
2025-03-04 09:10:39 +0700

    Normalize Migrate

--- database/migrations/2025_02_26_114439_add_timestamps_to_kesehatan.php
+++ database/migrations/2025_02_26_114439_add_timestamps_to_kesehatan.php
@@ -11,16 +11,26 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('kesehatan', function (Blueprint $table) {
+        Schema::create('kesehatan', function (Blueprint $table) {
+            $table->char('nomor_kk', 16); // Kolom nomor_kk dengan tipe char(16)
+            $table->string('ibu', 100); // Kolom ibu dengan tipe varchar(100)
+            $table->string('ayah', 100); // Kolom ayah dengan tipe varchar(100)
+            $table->string('telepon', 15)->nullable(); // Kolom telepon dengan tipe varchar(15), nullable
+            $table->string('anak_1', 100)->nullable(); // Kolom anak_1 dengan tipe varchar(100), nullable
+            $table->string('anak_2', 100)->nullable(); // Kolom anak_2 dengan tipe varchar(100), nullable
+            $table->text('alamat')->nullable(); // Kolom alamat dengan tipe text, nullable
             $table->timestamps(); // Menambahkan kolom created_at dan updated_at
+
+            // Menambahkan primary key pada kolom nomor_kk
+            $table->primary('nomor_kk');
         });
     }
     
+    /**
+     * Reverse the migrations.
+     */
     public function down()
     {
-        Schema::table('kesehatan', function (Blueprint $table) {
-            $table->dropTimestamps(); // Menghapus kolom created_at dan updated_at
-        });
+        Schema::dropIfExists('kesehatan'); // Menghapus tabel kesehatan
     }
-    
 };
