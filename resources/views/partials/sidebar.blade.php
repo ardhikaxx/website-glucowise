18642907fe71ccce8909859fa4457aeb51284039
ArizSaputra <as908244@gmail.com>
2025-03-05 14:48:33 +0700

    fix sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,78 +1,82 @@
 <!-- Sidebar Start -->
 <aside class="left-sidebar">
-    <!-- Sidebar scroll-->
-    <div>
-        <div class="brand-logo d-flex align-items-center justify-content-between">
-            <a href="./index.html" class="text-nowrap logo-img pt-3 ">
-                <img src="{{ asset('images/logos/favicon1.png') }}" width="150" alt="" />
+  <!-- Sidebar scroll-->
+  <div>
+      <div class="brand-logo d-flex align-items-center justify-content-between">
+          <a href="./index.html" class="text-nowrap logo-img pt-3 ">
+              <img src="{{ asset('images/logos/favicon1.png') }}" width="150" alt="" />
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+              <i class="ti ti-x fs-8"></i>
+          </div>
+      </div>
+      <!-- Sidebar navigation-->
+      <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+        <ul id="sidebarnav">
+          <li class="nav-small-cap">
+            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+            <span class="hide-menu">Home</span>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="/" aria-expanded="false">
+              <span>
+                <i class="ti ti-layout-dashboard"></i>
+              </span>
+              <span class="hide-menu">Dashboard</span>
             </a>
-            <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-                <i class="ti ti-x fs-8"></i>
-            </div>
-        </div>
-        <!-- Sidebar navigation-->
-        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-          <ul id="sidebarnav">
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">Home</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="/" aria-expanded="false">
-                <span>
-                  <i class="ti ti-layout-dashboard"></i>
-                </span>
-                <span class="hide-menu">Dashboard</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="/data_pengguna" aria-expanded="false">
-                <span>
-                  <i class="ti ti-file-description"></i>
-                </span>
-                <span class="hide-menu">Data Pengguna</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="/data-kesehatan" aria-expanded="false">
-                <span>
-                  <i class="ti ti-layout-dashboard"></i>
-                </span>
-                <span class="hide-menu">Data Kesehatan</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="/riwayat_kesehatan" aria-expanded="false">
-                <span>
-                  <i class="ti ti-layout-dashboard"></i>
-                </span>
-                <span class="hide-menu">Riwayat Kesehatan</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="/admin" aria-expanded="false">
-                <span>
-                  <i class="ti ti-layout-dashboard"></i>
-                </span>
-                <span class="hide-menu">Data Admin</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="/edukasi" aria-expanded="false">
-                <span>
-                  <i class="ti ti-layout-dashboard"></i>
-                </span>
-                <span class="hide-menu">Edukasi</span>
-              </a>
-            </li>
-            
-            
-          </ul>
-          
-        </nav>
-        <!-- End Sidebar navigation -->
-        <!-- End Sidebar navigation -->
-    </div>
-    <!-- End Sidebar scroll-->
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="/data_pengguna" aria-expanded="false">
+              <span>
+                <i class="ti ti-user"></i> <!-- Data Pengguna Icon -->
+              </span>
+              <span class="hide-menu">Data Pengguna</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="/data-kesehatan" aria-expanded="false">
+              <span>
+                <i class="ti ti-heart"></i> <!-- Data Kesehatan Icon -->
+              </span>
+              <span class="hide-menu">Data Kesehatan</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="/riwayat_kesehatan" aria-expanded="false">
+              <span>
+                <i class="ti ti-history"></i> <!-- Riwayat Kesehatan Icon -->
+              </span>
+              <span class="hide-menu">Riwayat Kesehatan</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="/data-screening" aria-expanded="false">
+              <span>
+                <i class="ti ti-clipboard"></i> <!-- Icon Data Screening -->
+              </span>
+              <span class="hide-menu">Data Screening</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="/edukasi" aria-expanded="false">
+              <span>
+                <i class="ti ti-book"></i> <!-- Edukasi Icon -->
+              </span>
+              <span class="hide-menu">Edukasi</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="/admin" aria-expanded="false">
+              <span>
+                <i class="ti ti-shield"></i> <!-- Data Admin Icon -->
+              </span>
+              <span class="hide-menu">Data Admin</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- End Sidebar navigation -->
+  </div>
+  <!-- End Sidebar scroll-->
 </aside>
-<!--  Sidebar End -->
+<!-- Sidebar End -->
