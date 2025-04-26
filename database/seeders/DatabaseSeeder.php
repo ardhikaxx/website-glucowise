b32beab4ee7cabd6ccd961f17f5ebdd135a1fba9
ArizSaputra <as908244@gmail.com>
2025-04-26 14:21:29 +0700

    Fixing Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,23 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            AdminSeeder::class,
+            DataKesehatanSeeder::class,
+            EdukasiSeeder::class,
+            HasilScreeningSeeder::class,
+            JawabanScreeningSeeder::class,
+            PenggunaSeeder::class,
+            PertanyaanScreeningSeeder::class,
+            RiwayatKesehatanSeeder::class,
+            TesScreeningSeeder::class,
+            UsersTableSeeder::class,
+        ]);
     }
 }
