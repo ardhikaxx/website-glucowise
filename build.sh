4695b45f93f4712d2d84c4915bc72626908bb1e0
ardhikaxx <ardhikayanuar58@gmail.com>
2025-05-27 22:34:23 +0700

    update- build script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Running deploy script"
+
+echo "[1/6] Pulling from GitHub"
+git pull origin
+
+echo "[2/6] Ensuring correct database host configuration"
+# Check that DB_HOST in .env matches the actual MySQL container name if using Docker
+# Example: DB_HOST=laravel_db (This should match your MySQL container name)
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=laravel_db/g' .env
+
+echo "[3/6] Creating database if one isn't found"
+touch database/database.sqlite
+
+echo "[4/6] Installing packages using composer"
+composer install
+
+echo "[5/6] Publishing API Platform assets"
+php artisan api-platform:install
+
+echo "[6/6] Running fresh migrations and seeding"
+php artisan migrate:fresh --seed
+
+echo "The app has been built and deployed!"
